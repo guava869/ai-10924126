d1955fa819e34a3481784f2c2e98319f5437b022
guava869 <71486118+guava869@users.noreply.github.com>
2020-12-18 15:07:51 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://guava869.github.io/ai-10924126/model.json';
 
 
 function preload() {
